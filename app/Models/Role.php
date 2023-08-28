5129925fed708b6fb40a6ac51925a4b2511dcc5c
tuanngoc1601 <tuanngoc12340@gmail.com>
2023-08-28 15:38:02 +0700

    create relationship models

---
+++ app/Models/Role.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class)
+            ->withTimestamps();
+    }
+}
