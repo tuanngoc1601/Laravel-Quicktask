9b93d0d21e98a50b253ea64b3ce4f1e330bd1598
tuanngoc1601 <tuanngoc12340@gmail.com>
2023-08-31 08:57:26 +0700

    quick fix

--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,8 @@ Route::delete('/users/{user}', [UserController::class, 'destroy'])
     ->name('users.destroy')
     ->middleware(['admin']);
 
+Route::resource('/tasks', TaskController::class);
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
